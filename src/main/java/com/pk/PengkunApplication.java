40b7ad55df86f1d6c6eaf3a64a95f31ca679248c
pengkun <2292759708@qq.com>
2021-11-17 21:40:34 +0800

    jenkins

---
+++ src/main/java/com/pk/PengkunApplication.java
@@ -0,0 +1,13 @@
+package com.pk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PengkunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PengkunApplication.class, args);
+    }
+
+}
